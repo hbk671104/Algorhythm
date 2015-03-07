664501c93c7c1e5e040e94263700bae02d8f7793
hbk671104 <hbk671104@gmail.com>
2015-03-07 15:51:07 -0500

    First commit

--- Algorhythm/PlaylistMasterViewController.swift
+++ Algorhythm/PlaylistMasterViewController.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class PlaylistMasterViewController : UIViewController {
 
+	@IBOutlet weak var aButton: UIButton!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+		aButton.setTitle("Press me!", forState:.Normal)
 	}
 
 	override func didReceiveMemoryWarning() {
@@ -20,6 +23,9 @@ class ViewController: UIViewController {
 		// Dispose of any resources that can be recreated.
 	}
 
+	@IBAction func buttonPressed() {
+		view.backgroundColor = UIColor.orangeColor()
+	}
 
 }
 
